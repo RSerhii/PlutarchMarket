8c42599ade0cfb9ce928617a631097d1e9a5a798
RSerhii <rubetsserhii@gmail.com>
2023-01-26 03:55:01 +0200

    Setup types

---
+++ src/Market/PTypes.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Market.PTypes (
+    POfferDatum (..),
+    POfferRedeemer (..),
+    POfferAction (..),
+) where
+
+import qualified GHC.Generics as GHC
+-- import Generics.SOP (Generic, I (I))
+
+import Plutarch
+-- import Plutarch.Api.V2.Contexts
+import Plutarch.DataRepr
+import Plutarch.Lift
+import Plutarch.Prelude
+import Plutarch.Api.V1 (PPubKeyHash)
+import Plutarch.Internal.PlutusType (pcon',pmatch')
+import Plutarch.Builtin (PIsData (..), pasInt, pforgetData)
+import Plutarch.Unsafe (punsafeCoerce)
+import PExtra.API
+import Market.Types
+
+-- PDatum
+
+newtype POfferDatum (s :: S)
+    = POfferDatum
+        ( Term
+            s
+            ( PDataRecord
+                '[ "token" ':= PAssetClass
+                 , "seller" ':= PPubKeyHash
+                 , "price" ':= PInteger
+                 ]
+            )
+        )
+    deriving stock (GHC.Generic)
+    deriving anyclass
+        (PIsData, PDataFields, PlutusType)
+
+instance DerivePlutusType POfferDatum where type DPTStrat _ = PlutusTypeData
+
+instance PUnsafeLiftDecl POfferDatum where type PLifted POfferDatum = OfferDatum
+deriving via (DerivePConstantViaData OfferDatum POfferDatum) instance (PConstantDecl OfferDatum)
+
+-- PRedeemer
+
+data POfferAction (s :: S) = PAcceptOffer | PCancelOffer
+
+instance PIsData POfferAction where
+    pfromDataImpl tx =
+        let x = pasInt # pforgetData tx
+         in pmatch' x pcon
+    pdataImpl x = pmatch x (punsafeCoerce . pdata . pcon')
+
+instance PlutusType POfferAction where
+    type PInner POfferAction = PInteger
+
+    pcon' PAcceptOffer  = 0
+    pcon' PCancelOffer = 1
+
+    pmatch' x f =
+        pif (x #== 0) (f PAcceptOffer) (f PCancelOffer)
+
+newtype POfferRedeemer (s :: S)
+    = POfferRedeemer
+        ( Term
+            s
+            ( PDataRecord
+                '[ "action" ':= POfferAction
+                 ]
+            )
+        )
+    deriving stock (GHC.Generic)
+    deriving anyclass
+        (PIsData, PDataFields, PlutusType)
+
+instance DerivePlutusType POfferRedeemer where type DPTStrat _ = PlutusTypeData
+
+instance PUnsafeLiftDecl POfferRedeemer where type PLifted POfferRedeemer = OfferRedeemer
+deriving via (DerivePConstantViaData OfferRedeemer POfferRedeemer) instance (PConstantDecl OfferRedeemer)
--- src/Market/Types.hs
+++ src/Market/Types.hs
@@ -1,4 +1,58 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module Market.Types
-  () where
+module Market.Types (
+  OfferDatum (..),
+  OfferRedeemer (..),
+  OfferAction (..),
+) where
+
+import PlutusLedgerApi.V1.Crypto (PubKeyHash)
+import PlutusLedgerApi.V1.Value
+import qualified PlutusTx
+import PlutusTx.Builtins
+import PlutusTx.IsData.Class
+    ( ToData(..), FromData(..), UnsafeFromData(..) )
+
+-- Datum
+
+data OfferDatum = OfferDatum
+    { token :: AssetClass
+    , seller :: PubKeyHash
+    , price :: Integer
+    }
+    deriving stock (Show)
+
+PlutusTx.makeIsDataIndexed ''OfferDatum [('OfferDatum, 0)]
+
+-- Redeemer
+
+
+data OfferAction = AcceptOffer | CancelOffer
+    deriving stock (Show)
+
+instance FromData OfferAction where
+    {-# INLINE fromBuiltinData #-}
+    fromBuiltinData d = matchData' d (\_ _ -> Nothing) (const Nothing) (const Nothing) chooseAction (const Nothing)
+      where
+        chooseAction i
+            | i == 0 = Just AcceptOffer
+            | i == 1 = Just CancelOffer
+            | otherwise = Nothing
+
+instance UnsafeFromData OfferAction where
+    {-# INLINE unsafeFromBuiltinData #-}
+    unsafeFromBuiltinData = maybe (Prelude.error "") id . fromBuiltinData
+
+instance ToData OfferAction where
+    {-# INLINE toBuiltinData #-}
+    toBuiltinData a = mkI $ case a of
+        AcceptOffer -> 0
+        CancelOffer -> 1
+
+data OfferRedeemer = OfferRedeemer
+    { action      :: OfferAction
+    }
+    deriving stock (Show)
+
+PlutusTx.makeIsDataIndexed ''OfferRedeemer [('OfferRedeemer, 0)]
